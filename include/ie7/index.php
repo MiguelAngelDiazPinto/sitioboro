2a716631a0647c9371e32511386b86b7793c15c7
miguelangeldiazpinto <mi.diazpinto@gmail.com>
2016-03-17 16:53:42 -0400

    cssincludejs

---
+++ include/ie7/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Untitled Document</title>
+<meta http-equiv="refresh" content="0;URL=/">
+</head>
+
+<body>
+</body>
+</html>
